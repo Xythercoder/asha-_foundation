915610db208eb1cb5303319010facac10f2eee8f
Xythercoder <rakeshsharmanine@gmail.com>
2025-04-13 18:12:59 +0530

    done

--- asha _foundation/website/settings.py
+++ asha _foundation/website/settings.py
@@ -135,10 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATIC_ROOT = '/var/www/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 LOGOUT_REDIRECT_URL = '/'
